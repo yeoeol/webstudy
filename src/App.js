82f5f69136a28520da31e350654cfbd7790f4ecd
annie4616 <annie4616@korea.ac.kr>
2024-03-06 22:11:17 +0900

    [Refactor] 코드 구조 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from "./logo.svg";
-import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainLayout from "./Layouts/MainLayout";
 import MainPage from "./Pages/MainPage";
 import EditPage from "./Pages/EditPage";
+import GlobalStyle from "./styles/GlobalStyle";
 function App() {
   return (
     <>
+      <GlobalStyle />
       <RouterComponent />
     </>
   );
@@ -19,8 +19,8 @@ const RouterComponent = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainLayout />}>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/edit/:boardNum" element={<EditPage />}></Route>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/edit/:boardNum" element={<EditPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
